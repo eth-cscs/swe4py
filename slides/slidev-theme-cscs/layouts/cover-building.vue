0b80081deb17fa6bb85ecf111712d4c0a1ee8689
DropD <rico.haeuselmann@gmail.com>
2025-03-18 16:35:55 +0100

    update theme to hopefully find cover pics

--- slides/slidev-theme-cscs/layouts/cover-building.vue
+++ slides/slidev-theme-cscs/layouts/cover-building.vue
@@ -2,8 +2,9 @@
   <div class="slidev-layout cover">
     <TitleTop />
     <div class="my-auto w-full below-header">
-      <img src="../assets/cover-machine.png" class="banner"/>
+      <img src="../assets/cover-building.png" class="banner"/>
       <slot />
     </div>
   </div>
 </template>
+
