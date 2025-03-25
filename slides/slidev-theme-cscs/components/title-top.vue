15ba8718467c52ee7b3fef6ba1d68f3472d62ffb
Rico Häuselmann <rico.haeuselmann@gmail.com>
2025-03-25 13:06:21 +0100

    update theme

--- slides/slidev-theme-cscs/components/title-top.vue
+++ slides/slidev-theme-cscs/components/title-top.vue
@@ -1,11 +1,12 @@
 <template>
-  <img src="../assets/CSCS_logo.jpg" class="absolute top-0 left-0 p-1 h-18" />
   <LightOrDark>
     <template #light>
-      <img src="../assets/eth_logo_kurz_pos.png" class="absolute top-0 right-0 p-1 h-18" />
+      <img src="../assets/CSCS_logo.png" class="absolute top-0 left-10 p-1 h-18 logo" />
+      <img src="../assets/eth_logo_kurz_pos.png" class="absolute top-0 right-10 p-1 h-18 logo" />
     </template>
     <template #dark>
-      <img src="../assets/eth_logo_kurz_neg.png" class="absolute top-0 right-0 p-1 h-18" />
+      <img src="../assets/CSCS_logo_neg.png" class="absolute top-0 left-10 p-1 h-18 logo" />
+      <img src="../assets/eth_logo_kurz_neg.png" class="absolute top-0 right-10 p-1 h-18 logo" />
     </template>
   </LightOrDark>
 </template>
--- slides/slidev-theme-cscs/layouts/cover.vue
+++ slides/slidev-theme-cscs/layouts/cover.vue
@@ -2,7 +2,10 @@
   <div class="slidev-layout cover">
     <TitleTop />
     <div class="my-auto w-full below-header">
-      <img src="../assets/cover-machine.png" class="banner"/>
+      <LightOrDark>
+        <template #light><img src="../assets/cover-machine.png" class="banner"/></template>
+        <template #dark><img src="../assets/cover-building.png" class="banner"/></template>
+      </LightOrDark>
       <slot />
     </div>
   </div>
